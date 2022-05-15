6f3381510c7a13d436eda4ddfa25e86e7613a0e9
Isabella Melo <isacpmelo@gmail.com>
2022-05-15 20:06:11 -0300

    Inicialização do Projeto

---
+++ conversor-criptomoedas/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <body class="">
+    <div class="w-1/2 mx-auto my-20 ">
+      <h1 class="text-3xl font-bold text-indigo-800">Conversor de Criptomoedas</h1>
+      <article class="my-20">
+        <h2 class="text-5xl font-semibold mb-5">Conversor de Criptomoedas</h2>
+        <p class="mb-5">Saiba quanto cada criptomoeda está valendo em dólar.</p>
+      </article>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  head() {
+    return {
+      title: 'Conversor de Criptomoedas',
+    }
+  }
+}
+</script>
